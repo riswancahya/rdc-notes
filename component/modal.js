b727ddcff1c18c513f27828bd06922f624f4b9d6
Riswan <cahya691@gmail.com>
2020-11-21 21:26:33 +0700

    paksa install

--- component/modal.js
+++ component/modal.js
@@ -34,7 +34,7 @@ export default function Modal({toggle, confirm, title, description, toggleConfir
                     </button>: <button className="a2hs-button w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
                         {confirm}    
                     </button>}
-                    <button type="button" onClick={()=>toggle(false)} className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                    <button type="button" onClick={()=>toggle(false)} className="a2hs-button-cancel mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                         Cancel
                     </button>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,31 @@ import Content from '../component/content'
 import Modal from '../component/modal'
 
 export default function Home() {
-    const [modal, setModal] = useState(true)
     const installEl = useRef(null)
     useEffect(()=>{
 
-        const check = localStorage.getItem('install') 
+        let deferredPrompt;
+        let addArea = installEl.current;
+        addArea.style.display = 'none';
+        let addBtn = document.querySelector('.a2hs-button');
+        let cancelBtn = document.querySelector('.a2hs-button-cancel')
+        addBtn.style.display = 'none';
 
-        if (check !== 'false'){
-            let deferredPrompt;
-            let addArea = installEl.current;
+        cancelBtn.addEventListener('click', (e) => {
             addArea.style.display = 'none';
-            let addBtn = document.querySelector('.a2hs-button');
             addBtn.style.display = 'none';
+        })
 
-            window.addEventListener('beforeinstallprompt', (e) => {
-                // Prevent Chrome 67 and earlier from automatically showing the prompt
-                e.preventDefault();
-                // Stash the event so it can be triggered later.
-                deferredPrompt = e;
-                // Update UI to notify the user they can add to home screen
-                addBtn.style.display = 'block';
-                addArea.style.display = 'block';
-            
-                addBtn.addEventListener('click', (e) => {
+        window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent Chrome 67 and earlier from automatically showing the prompt
+            e.preventDefault();
+            // Stash the event so it can be triggered later.
+            deferredPrompt = e;
+            // Update UI to notify the user they can add to home screen
+            addBtn.style.display = 'block';
+            addArea.style.display = 'block';
+        
+            addBtn.addEventListener('click', (e) => {
                 // hide our user interface that shows our A2HS button
                 addBtn.style.display = 'none';
                 addArea.style.display = 'none';
@@ -41,36 +43,27 @@ export default function Home() {
                         console.log('User dismissed the A2HS prompt');
                     }
                     deferredPrompt = null;
-                    });
                 });
             });
+        });
 
-        } else {
-            setModal(false)
-        }
+    
 
     },[])
-
-    const toggle = (value) => {
-        if (!value) {
-            localStorage.setItem('install', false)
-            setModal(false)
-        }
-    }
     
     return (<React.Fragment>
         <div className="grid grid-cols-1 lg:grid-cols-4 bg-gray-50 font-sans">
             <Sidebar />
             <Content />
         </div>
-        {modal && <div ref={installEl} className="fixed font-sans h-0">
+        <div ref={installEl} className="fixed font-sans h-0 hidden">
             <Modal 
                 title="Install PWA" 
                 description="Akses lebih mudah melalui layar beranda anda tanpa koneksi internet" 
-                toggle={toggle} 
+                toggle={true} 
                 confirm="Install" 
                 install={true}
             />
-        </div>}
+        </div>
     </React.Fragment>)
 }
